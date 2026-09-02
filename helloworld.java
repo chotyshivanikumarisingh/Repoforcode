0345fe98840ef5af0c58c6c98828089e54e13548
shivani kumari <65593161+chotyshivanikumarisingh@users.noreply.github.com>
2026-09-02 20:39:01 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,8 +1,8 @@
-public class HelloWorld {
+public class helloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        System.out.println("hello World");
 
     }
 }
